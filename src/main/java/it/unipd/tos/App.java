c201f0f50dc4b3a7d97a6060134c022f636b4761
Egon Galvani <egongalvani@libero.it>
2019-12-03 17:04:13 +0100

    close #2 - implementato sconto sul panino meno costoso

--- src/main/java/it/unipd/tos/App.java
+++ src/main/java/it/unipd/tos/App.java
@@ -5,8 +5,11 @@
 package it.unipd.tos;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import it.unipd.tos.business.exception.TakeAwayBillException;
+import it.unipd.tos.business.model.ItemType;
 import it.unipd.tos.business.model.MenuItem;
 import it.unipd.tos.business.model.TakeAwayBill;
 
@@ -15,9 +18,18 @@ public class App implements TakeAwayBill {
     @Override
     public double getOrderPrice(List<MenuItem> itemsOrdered) throws TakeAwayBillException {
         
+        // calcolo del totale
         double orderPrice = 0.0D; 
         for(MenuItem item : itemsOrdered) {
             orderPrice += item.getPrice(); 
+        }
+        
+        // sconto per ordini con più di 5 panini 
+        List<MenuItem> panini = itemsOrdered.stream()
+                                .filter(e -> e.getItemType() == ItemType.PANINO)
+                                .collect(Collectors.toList());  
+        if(panini.size() > 5) {
+            orderPrice -= 0.5 * panini.stream().mapToDouble(MenuItem::getPrice).min().getAsDouble();
         }
         
         return orderPrice; 
